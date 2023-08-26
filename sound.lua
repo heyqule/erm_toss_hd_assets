cbda970263ddb8cd510ecef7197b68aa5a2590fd
heyqule <hq.jfax@gmail.com>
2023-08-26 13:56:04 -0400

    Doc updates

--- sound.lua
+++ sound.lua
@@ -7,150 +7,175 @@
 --
 local TossSound = {}
 
-function TossSound.enemy_death(name, volume)
+function TossSound.enemy_death(name, volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/" .. name .. "/death.ogg",
         volume = volume
     }
 end
 
-function TossSound.ball_attack(volume)
+function TossSound.ball_attack(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/ball_attack_shared.ogg",
         volume = volume
     }
 end
 
-function TossSound.scout_attack_missile(volume)
+function TossSound.scout_attack_missile(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/scout/scout_attack_missile.ogg",
         volume = volume
     }
 end
 
-function TossSound.scout_attack_pulse(volume)
+function TossSound.scout_attack_pulse(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/scout/scout_attack_pulse.ogg",
         volume = volume
     }
 end
 
-function TossSound.interceptor_attack(volume)
+function TossSound.interceptor_attack(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/carrier/interceptor.ogg",
         volume = volume
     }
 end
 
-function TossSound.interceptor_projectile(volume)
+function TossSound.interceptor_projectile(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/interceptor/interceptor_projectile.ogg",
         volume = volume
     }
 end
 
-function TossSound.corsair_attack(volume)
+function TossSound.corsair_attack(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/corsair/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.darktemplar_attack(volume)
+function TossSound.darktemplar_attack(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/darktemplar/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.templar_attack(volume)
+function TossSound.templar_attack(volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
 
         filename = "__erm_toss_hd_assets__/sound/enemies/templar/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.archon_attack(volume)
+function TossSound.archon_attack(volume, distance)
+    distance = distance or 1
     return {
-
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/archon/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.darkarchon_attack(volume)
+function TossSound.darkarchon_attack(volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
 
         filename = "__erm_toss_hd_assets__/sound/enemies/darkarchon/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.probe_attack(volume)
+function TossSound.probe_attack(volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
 
         filename = "__erm_toss_hd_assets__/sound/enemies/probe/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.zealot_attack(volume)
-    return
-    {
+function TossSound.zealot_attack(volume, distance)
+    distance = distance or 1
+    return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/zealot/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.scarab_attack(volume)
-    return
-    {
+function TossSound.scarab_attack(volume, distance)
+    distance = distance or 1
+    return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/scarab/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.reaver_attack(volume)
-    return
-    {
+function TossSound.reaver_attack(volume, distance)
+    distance = distance or 1
+    return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/reaver/attack.ogg",
         volume = volume
     }
 end
 
-function TossSound.shuttle_drop(volume)
-    return
-    {
+function TossSound.shuttle_drop(volume, distance)
+    distance = distance or 1
+    return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/enemies/shuttle/shuttle_drop.ogg",
         volume = volume
     }
 end
 
-function TossSound.building_dying_sound(volume)
+function TossSound.building_dying_sound(volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/buildings/explode_building.ogg",
         volume = volume
     }
 end
 
-function TossSound.shield_battery_ability(volume)
+function TossSound.shield_battery_ability(volume, distance)
+    distance = distance or 1
     return {
+        audible_distance_modifier = distance,
         filename = "__erm_toss_hd_assets__/sound/buildings/shield_battery_ability.ogg",
         volume = volume
     }
 end
 
-function TossSound.shield_battery_idle(volume)
+function TossSound.shield_battery_idle(volume, distance)
+    distance = distance or 1
     return {
-        audible_distance_modifier = 0.5,
+        audible_distance_modifier = distance,
         sound = {
             {
                 filename = "__erm_toss_hd_assets__/sound/buildings/shield_battery.ogg",
@@ -162,9 +187,10 @@ function TossSound.shield_battery_idle(volume)
     }
 end
 
-function TossSound.cannon_idle(volume)
+function TossSound.cannon_idle(volume, distance)
+    distance = distance or 1
     return {
-        audible_distance_modifier = 0.5,
+        audible_distance_modifier = distance,
         sound = {
             {
                 filename = "__erm_toss_hd_assets__/sound/buildings/cannon.ogg",
@@ -176,10 +202,10 @@ function TossSound.cannon_idle(volume)
     }
 end
 
-function TossSound.building_working_sound(name, volume)
-    return
-    {
-        audible_distance_modifier = 1,
+function TossSound.building_working_sound(name, volume, distance)
+    distance = distance or 1
+    return {
+        audible_distance_modifier = distance,
         max_sounds_per_type = 3,
         sound = {
             filename = "__erm_toss_hd_assets__/sound/buildings/" .. name .. ".ogg",
