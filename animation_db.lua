bce1909c64d50f531baa30186cf31ac66da060aa
heyqule <hq.jfax@gmail.com>
2024-10-29 12:08:47 -0400

    Convert to double quote for consistency

--- animation_db.lua
+++ animation_db.lua
@@ -4,7 +4,7 @@
 --- DateTime: 7/20/2023 6:26 PM
 ---
 
-local AnimationDB = require('__erm_libs__/prototypes/animation_db')
+local AnimationDB = require("__erm_libs__/prototypes/animation_db")
 
 --- Naming convention EntityType_Name. EntityType follows the graphics folder structure.
 --- e.g [unit][zergling], [building][hive], [projectiles][spore], etc
@@ -12,40 +12,40 @@ local AnimationDB = require('__erm_libs__/prototypes/animation_db')
 --- death and projectile are handled differently because they are mostly single layer animation.
 AnimationDB.data = {
     buildings = {
-        arbiter_tribunal = require('__erm_toss_hd_assets__/db/buildings/arbiter_tribunal'),
-        cannon = require('__erm_toss_hd_assets__/db/buildings/cannon'),
-        citadel_adun = require('__erm_toss_hd_assets__/db/buildings/citadel_adun'),
-        cybernetics_core = require('__erm_toss_hd_assets__/db/buildings/cybernetics_core'),
-        fleet_beacon = require('__erm_toss_hd_assets__/db/buildings/fleet_beacon'),
-        forge = require('__erm_toss_hd_assets__/db/buildings/forge'),
-        gateway = require('__erm_toss_hd_assets__/db/buildings/gateway'),
-        nexus = require('__erm_toss_hd_assets__/db/buildings/nexus'),
-        pylon = require('__erm_toss_hd_assets__/db/buildings/pylon'),
-        robotics_facility = require('__erm_toss_hd_assets__/db/buildings/robotics_facility'),
-        robotics_support_bay = require('__erm_toss_hd_assets__/db/buildings/robotics_support_bay'),
-        shield_battery = require('__erm_toss_hd_assets__/db/buildings/shield_battery'),
-        stargate = require('__erm_toss_hd_assets__/db/buildings/stargate'),
-        templar_archive = require('__erm_toss_hd_assets__/db/buildings/templar_archive'),
-        warpgate = require('__erm_toss_hd_assets__/db/buildings/warpgate'),
+        arbiter_tribunal = require("__erm_toss_hd_assets__/db/buildings/arbiter_tribunal"),
+        cannon = require("__erm_toss_hd_assets__/db/buildings/cannon"),
+        citadel_adun = require("__erm_toss_hd_assets__/db/buildings/citadel_adun"),
+        cybernetics_core = require("__erm_toss_hd_assets__/db/buildings/cybernetics_core"),
+        fleet_beacon = require("__erm_toss_hd_assets__/db/buildings/fleet_beacon"),
+        forge = require("__erm_toss_hd_assets__/db/buildings/forge"),
+        gateway = require("__erm_toss_hd_assets__/db/buildings/gateway"),
+        nexus = require("__erm_toss_hd_assets__/db/buildings/nexus"),
+        pylon = require("__erm_toss_hd_assets__/db/buildings/pylon"),
+        robotics_facility = require("__erm_toss_hd_assets__/db/buildings/robotics_facility"),
+        robotics_support_bay = require("__erm_toss_hd_assets__/db/buildings/robotics_support_bay"),
+        shield_battery = require("__erm_toss_hd_assets__/db/buildings/shield_battery"),
+        stargate = require("__erm_toss_hd_assets__/db/buildings/stargate"),
+        templar_archive = require("__erm_toss_hd_assets__/db/buildings/templar_archive"),
+        warpgate = require("__erm_toss_hd_assets__/db/buildings/warpgate"),
     },
-    death = require('__erm_toss_hd_assets__/db/death'),
-    projectiles = require('__erm_toss_hd_assets__/db/projectiles'),
+    death = require("__erm_toss_hd_assets__/db/death"),
+    projectiles = require("__erm_toss_hd_assets__/db/projectiles"),
     units = {
-        arbiter = require('__erm_toss_hd_assets__/db/units/arbiter'),
-        archon = require('__erm_toss_hd_assets__/db/units/archon'),
-        carrier = require('__erm_toss_hd_assets__/db/units/carrier'),
-        corsair = require('__erm_toss_hd_assets__/db/units/corsair'),
-        darkarchon = require('__erm_toss_hd_assets__/db/units/darkarchon'),
-        darktemplar = require('__erm_toss_hd_assets__/db/units/darktemplar'),
-        dragoon = require('__erm_toss_hd_assets__/db/units/dragoon'),
-        interceptor = require('__erm_toss_hd_assets__/db/units/interceptor'),
-        probe = require('__erm_toss_hd_assets__/db/units/probe'),
-        reaver = require('__erm_toss_hd_assets__/db/units/reaver'),
-        scarab = require('__erm_toss_hd_assets__/db/units/scarab'),
-        shuttle = require('__erm_toss_hd_assets__/db/units/shuttle'),
-        scout = require('__erm_toss_hd_assets__/db/units/scout'),
-        templar = require('__erm_toss_hd_assets__/db/units/templar'),
-        zealot = require('__erm_toss_hd_assets__/db/units/zealot'),
+        arbiter = require("__erm_toss_hd_assets__/db/units/arbiter"),
+        archon = require("__erm_toss_hd_assets__/db/units/archon"),
+        carrier = require("__erm_toss_hd_assets__/db/units/carrier"),
+        corsair = require("__erm_toss_hd_assets__/db/units/corsair"),
+        darkarchon = require("__erm_toss_hd_assets__/db/units/darkarchon"),
+        darktemplar = require("__erm_toss_hd_assets__/db/units/darktemplar"),
+        dragoon = require("__erm_toss_hd_assets__/db/units/dragoon"),
+        interceptor = require("__erm_toss_hd_assets__/db/units/interceptor"),
+        probe = require("__erm_toss_hd_assets__/db/units/probe"),
+        reaver = require("__erm_toss_hd_assets__/db/units/reaver"),
+        scarab = require("__erm_toss_hd_assets__/db/units/scarab"),
+        shuttle = require("__erm_toss_hd_assets__/db/units/shuttle"),
+        scout = require("__erm_toss_hd_assets__/db/units/scout"),
+        templar = require("__erm_toss_hd_assets__/db/units/templar"),
+        zealot = require("__erm_toss_hd_assets__/db/units/zealot"),
     }
 
 }
